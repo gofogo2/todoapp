6061f71cf38cc1662e29df5a275c6ae654153709
gofogo <promoted.go@gmail.com>
2022-12-22 17:42:16 +0900

    add: layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 function App() {
   return (
     <div className="App">
-      <div className="bg-green-100" >Helloworld</div>
+      <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-br from-slate-600 to-slate-800" >
+        <div className="bg-[#24253F] w-[20rem] h-[20rem] rounded-2xl flex flex-col">
+          <div className="flex-[0.4] bg-[#2f253F] rounded-t-2xl" ></div>
+          <div className="flex-1 bg-[#24253F]" ></div>
+          <div className="flex-[0.3] bg-[#2f253F]  rounded-b-2xl" ></div>
+        </div>
+      </div>
     </div>
   );
 }
